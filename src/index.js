371f890d5e0dc85e0650ea5fcf5b85b39b441d67
Sauvic <sauvicpaulchy@gmail.com>
2023-01-21 14:36:08 +0530

    First commit with basic setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+const db = require("./models/index");
+
+const setupAndStartServer = () => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
+  app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
+
+setupAndStartServer();
