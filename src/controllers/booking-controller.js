5540d959cee8a809a9d574758029287286b5e2bc
Sauvic <sauvicpaulchy@gmail.com>
2023-01-29 14:25:23 +0530

    feat: basic setup for rabbit mq done

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -14,8 +14,16 @@ class BookingController {
 
   async sendMessageToQueue(req, res) {
     const channel = await createChannel();
-    const data = { message: "Success" };
-    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    const payload = {
+      data: {
+        subject: "This is a noti from queue",
+        content: "Some queue will subscribe this",
+        recepientEmail: "someuserEmail@gmail.com",
+        notificationTime: "2023-01-28T12:44:06",
+      },
+      service: "CREATE_TICKET",
+    };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
     return res.status(200).json({
       message: "Successfully published the event",
     });
